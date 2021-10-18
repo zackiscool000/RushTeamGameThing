f7d356e9ef6542fa63674cc122c539d5a62e7440
zackiscool000 <90630104+zackiscool000@users.noreply.github.com>
2021-10-18 10:04:40 -0400

    Update Instance_2.0.1.js

--- Instance_2.0.1.js
+++ Instance_2.0.1.js
@@ -4,7 +4,7 @@ UnityLoader.Error.handler = function(){
 console.log("Error Happened !");
 
 }
-var unityInstance = UnityLoader.instantiate("gameContainer", "Build/2.0.1/Release/build2.0.1.json", {
+var unityInstance = UnityLoader.instantiate("gameContainer", "https://raw.githubusercontent.com/zackiscool000/RushTeamGameThing/main/build2.0.1.json", {
 onProgress: UnityProgress,
 Module: {
 onRuntimeInitialized: function() { UnityProgress(unityInstance, "complete") }
